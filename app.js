b9c3c785b5a2c096db9dfb9026fcd244a96c50e5
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-06-10 09:17:17 -0500

    made small changes

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ console.log("Hello World!\n==========\n");
 console.log("EXERCISE 1:\n==========\n");
 
 function printOdds(count) {
+  if (count < 0) {
+    console.log("Please enter a postive number");
+    return;
+  }
+
   for (let i = 0; i <= count; i++) {
     if (i % 2 != 0) {
       console.log(i);
@@ -12,11 +17,20 @@ function printOdds(count) {
 }
 
 printOdds(100);
+printOdds(-100);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+
+
 function checkAge(userName, age) {
+  
+  if (!userName || !age) {
+  console.log("Please provide both a name and age");
+  return;
+}
+
   let aboveSixteen = `Congrats ${userName}, you can drive!`;
   let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`;
 
@@ -26,5 +40,7 @@ function checkAge(userName, age) {
     console.log(aboveSixteen);
   }
 }
+checkAge();
+checkAge(15);
 checkAge("kethomas7", 27);
 checkAge("Cyd", 13);
