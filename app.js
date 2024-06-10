e7aa58375290c84adad17294f223712302711c8e
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-06-09 22:01:38 -0500

    Completed exercises 1 and 2

--- app.js
+++ app.js
@@ -3,5 +3,28 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) {
+  for (let i = 0; i <= count; i++) {
+    if (i % 2 != 0) {
+      console.log(i);
+    }
+  }
+}
+
+printOdds(100);
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(userName, age) {
+  let aboveSixteen = `Congrats ${userName}, you can drive!`;
+  let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`;
+
+  if (age < 16) {
+    console.log(belowSixteen);
+  } else {
+    console.log(aboveSixteen);
+  }
+}
+checkAge("kethomas7", 27);
+checkAge("Cyd", 13);
